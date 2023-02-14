1289c9000c630c7887390ae705b23465c1512e5e
Chris Wilson <chrisw@yepher.com>
2023-02-14 08:46:15 -0600

    Fixes error when running `setup.sh name` on MacOS M2

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-find . -type f -exec sed -i 's/{dockerHubUsername}/'$1'/g' {} +
+
+if [ $# -eq 0 ]; then
+  echo "Please provide a value for {dockerHubUsername}"
+  exit 1
+fi
+
+#find . -name '*.yaml' -type f -exec sed -i "s/{dockerHubUsername}/$1/g" {} +
+#find . -name '*.yaml' -type f -exec sed -i "s|{dockerHubUsername}|$1|g" {} +
+#find . -name '*.yaml' -type f -exec sed -i -E "s/{dockerHubUsername}/$1/g" {} +
+find . -name '*.yaml' -type f -exec sed -i '' -E "s/{dockerHubUsername}/$1/g" {} +
+
+
